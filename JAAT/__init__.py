37790827f2b66585d80a4d0e03d95ccb9415672b
Stephen Meisenbacher <sjmeis@gtgd.com>
2026-04-17 17:34:56 +0200

    suppress correctly

--- JAAT/__init__.py
+++ JAAT/__init__.py
@@ -1,13 +1,14 @@
-from .match import TaskMatch, SkillMatch, ActivityMatch, AIMatch
-from .extract import FirmExtract, WageExtract, Readability
-from .label import CREAM, JobTag
-from .search import ConceptSearch
 import os
-
 os.environ["USE_TORCH"] = "1"
 os.environ["USE_TF"] = "0"
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+
+from .match import TaskMatch, SkillMatch, ActivityMatch, AIMatch
+from .extract import FirmExtract, WageExtract, Readability
+from .label import CREAM, JobTag
+from .search import ConceptSearch
+
 
 __all__ = [
     "TaskMatch", "SkillMatch", "ActivityMatch", "AIMatch",
