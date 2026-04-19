ccb33d7bf15d65d005b57aa4a91f54d26d5aef98
Stephen Meisenbacher <sjmeis@gtgd.com>
2026-04-19 17:07:24 +0200

    add typing, fix bugs

--- JAAT/label.py
+++ JAAT/label.py
@@ -1,3 +1,4 @@
+from typing import List, Union, Tuple, Dict, Optional
 import pandas as pd
 import torch
 import multiprocessing as mp
@@ -19,25 +20,28 @@ from .base import get_device_settings, logger
 tqdm.pandas()
 
 CLEAN_RE = re.compile(r'[^a-z0-9]+')
-
-def get_context(text, keywords, n):
-    text = CLEAN_RE.sub(' ', text.lower())
-
-    words = text.split()
-    k_set = {k.strip() for k in keywords}
-    found_index = [i for i, w in enumerate(words) if any(k in w for k in k_set)]
-    context = [" ".join(words[max(0, idx-n):min(idx+1, len(words))]) for idx in found_index]
-
-    if len(context) > 0:
-        return context
-    else:
+    
+def get_context(text: str, keywords: List[str], n: int) -> Optional[List[str]]:
+    if not text:
         return None
     
+    text = CLEAN_RE.sub(' ', text.lower())
+    words = text.split()
+    k_set = {k.strip() for k in keywords}
+    found_indices = [i for i, w in enumerate(words) if any(k in w for k in k_set)]
+    
+    if not found_indices:
+        return None
+        
+    return [" ".join(words[max(0, idx-n):min(idx+n+1, len(words))]) for idx in found_indices]
+    
+clf = None
+
 def init_pool(classifier):
     global clf
     clf = classifier
 
-def classify(contexts):
+def classify(contexts: List[Union[str, None]]) -> int:
     global clf
     if contexts is None:
         return 0
@@ -45,12 +49,12 @@ def classify(contexts):
     return 1 if sum(res) > 0 else 0
 
 class CREAM():
-    def __init__(self, keywords, rules, class_name="label", n=4, threshold=0.9):
+    def __init__(self, keywords: List[str], rules: List[Tuple[str, any]], class_name: str = "label", n: int = 4, threshold: float = 0.9):
         if not isinstance(keywords, list) or not isinstance(rules, list):
             logger.error("ERROR: keywords and rules must be given as a list of values. \n KEYWORDS: [k1, k2, ..., kn] \n RULES: [(rule_1, label), (rule_2, label), ..., (rule_n, label)]")
             return
         
-        logger.info("Initalizing CREAM...", flush=True)
+        logger.info("Initalizing CREAM...")
         self.device, _ = get_device_settings()
 
         self.class_name = class_name
@@ -59,7 +63,7 @@ class CREAM():
         self.keywords = keywords
         rules = [(k, 0) for k in self.keywords] + rules
         self.rules = pd.DataFrame(rules, columns=["rule", self.class_name])
-        self.rule_texts = self.rules_df['rule'].tolist()
+        self.rule_texts = self.rules['rule'].tolist()
 
         self.model = SentenceTransformer("thenlper/gte-large", device=self.device)
 
@@ -67,28 +71,18 @@ class CREAM():
         self.encoded_rules = torch.nn.functional.normalize(self.encoded_rules, p=2, dim=1)
         self.rule_map = dict(zip(self.rule_texts, self.rules[self.class_name].tolist()))
 
-        logger.info("Finished.", flush=True)
+        logger.info("Finished.")
 
-    def get_sim(self, q):
+    def get_sim(self, q: str) -> Dict:
         sim_scores = util.cos_sim(self.model.encode([q]), self.encoded_rules)
         return dict(zip(self.rule_map.keys(), sim_scores[0].tolist()))
     
-    def label_from_max(self, scores):
+    def label_from_max(self, scores: Dict) -> Tuple[str, str, float]:
         max_rule = max(scores, key=scores.get)
         label = self.rule_map[max_rule]
         return max_rule, label, scores[max_rule]
-    
-    def get_context(self, text):
-        text = text.lower()
-        text = re.sub(r'[^a-z0-9]+', ' ', text)
-
-        words = text.split()
-        found_index = [i for i, w in enumerate(words) if any(k.strip() in w for k in self.keywords)]
-        context = [" ".join(words[max(0, idx-self.n):min(idx+self.n+1, len(words))]) for idx in found_index]
-
-        return '|'.join(context)
         
-    def run(self, texts):
+    def run(self, texts: List[str]) -> pd.DataFrame:
         all_contexts = []
         text_indices = []
         
@@ -107,22 +101,22 @@ class CREAM():
         sim_matrix = torch.mm(embeddings, self.encoded_rules.T)
         
         max_scores, max_indices = torch.max(sim_matrix, dim=1)
-        max_scores = max_scores.cpu().numpy()
-        max_indices = max_indices.cpu().numpy()
+        max_scores = max_scores.cpu().tolist()
+        max_indices = max_indices.cpu().tolist()
 
         results = [ (None, 0, 0.0) ] * len(texts)
         
         for idx, score, rule_idx in zip(text_indices, max_scores, max_indices):
             if score >= self.threshold:
                 if score > results[idx][2]:
-                    results[idx] = (self.rule_texts[rule_idx], self.rule_labels[rule_idx], score)
+                    results[idx] = (self.rule_texts[rule_idx], self.rule_labels[rule_idx], round(score, 3))
 
         df = pd.DataFrame(texts, columns=["text"])
         df['inferred_rule'], df['inferred_label'], df['inferred_confidence'] = zip(*results)
         return df
     
 class JobTag():
-    def __init__(self, class_name, n=4):
+    def __init__(self, class_name: str, n: int = 4) -> None:
         with open(impresources.files("JAAT.data") / "keywords.json", 'r') as f:
             self.keywords = json.load(f)
 
@@ -153,15 +147,15 @@ class JobTag():
             self.clf = compress_pickle.load(f, compression="lzma", set_default_extension=False)
         self.n = n
 
-    def get_tag(self, text):
+    def get_tag(self, text: str) -> Tuple[str, int]:
         contexts = get_context(text, keywords=self.keywords[self.class_name], n=self.n)
         if contexts is None:
             return (self.class_name, 0)
         res = self.clf.predict(contexts)
         return (self.class_name, 1 if sum(res) > 0 else 0)
     
-    def get_tag_batch(self, texts, progress_bar=False):
-        with mp.Pool(mp.cpu_count(), init_pool(self.clf)) as pool:
+    def get_tag_batch(self, texts: List[str], progress_bar: bool = False) -> List[int]:
+        with mp.Pool(mp.cpu_count(), initializer=init_pool, initargs=(self.clf,)) as pool:
             c = pool.imap(partial(get_context, keywords=self.keywords[self.class_name], n=self.n), texts)
             all_contexts = list(c)
             if progress_bar == True:
