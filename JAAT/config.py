91fa62ae06e7a64147ef0556da3c64ce21cb11e9
Stephen Meisenbacher <sjmeis@gtgd.com>
2026-06-08 22:17:57 +0200

    increment

--- JAAT/config.py
+++ JAAT/config.py
@@ -17,7 +17,7 @@ GLOBAL_SETTINGS = {
 
 MODEL_CACHE = {}
 
-VERSION = "1.1.0"
+VERSION = "1.1.1"
 COPYRIGHT = "© 2025-2026 Loyola University Chicago. All rights reserved."
 DESCRIPTION = "An NLP-powered toolkit for job ad text analysis."
 
